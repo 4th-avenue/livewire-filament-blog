9161f2f4e8f784cb503eed61c2424af3f89a36e9
4th-avenue <quebec706@gmail.com>
2024-10-29 18:58:15 +0900

    Seeder를 사용해 categories와 posts 테이블에 가상의 데이터 만들기

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Post::factory(100)->create();
+        Category::factory(5)->create();
 
         User::factory()->create([
             'name' => 'Test User',
