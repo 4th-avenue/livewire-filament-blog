e49117ce7db829f9d1d46def968bbd9f28a67564
4th-avenue <quebec706@gmail.com>
2024-11-02 16:01:31 +0900

    검색 기능 구현

--- app/Livewire/SearchBox.php
+++ app/Livewire/SearchBox.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class SearchBox extends Component
 {
+    public $search = '';
+
+    public function updatedSearch()
+    {
+        $this->dispatch('search', search: $this->search);
+    }
+
     public function render()
     {
         return view('livewire.search-box');
