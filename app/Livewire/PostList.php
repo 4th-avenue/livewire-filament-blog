8351c9ba046f6e6f5413d5f1d37dc669477021b9
4th-avenue <quebec706@gmail.com>
2024-11-02 14:49:33 +0900

    Post 목록 최신순과 오래된순 정렬 기능 구현

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
 use Livewire\Attributes\Computed;
 
@@ -11,10 +12,18 @@ class PostList extends Component
 {
     use WithPagination;
 
+    #[Url()]
+    public $sort = 'desc';
+
+    public function setSort($sort)
+    {
+        $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+    }
+
     #[Computed()]
     public function posts()
     {
-        return Post::published()->orderBy('published_at', 'desc')->paginate(3);
+        return Post::published()->orderBy('published_at', $this->sort)->paginate(3);
     }
 
     public function render()
