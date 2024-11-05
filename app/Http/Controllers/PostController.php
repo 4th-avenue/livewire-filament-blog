013c3e6554fb97205ac5fca1a73567fc11cb7199
4th-avenue <quebec706@gmail.com>
2024-11-05 13:45:21 +0900

    우측 사이드바에 Post의 Category 목록 표시하기

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,7 +10,9 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::take(5)->get()
+            'categories' => Category::whereHas('posts', function ($query) {
+                $query->published();
+            })->take(10)->get()
         ]);
     }
 }
