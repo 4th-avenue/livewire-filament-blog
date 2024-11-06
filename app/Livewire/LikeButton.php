a41879cf7eb23a2bc175fdcb02efa52f0704a4b4
4th-avenue <quebec706@gmail.com>
2024-11-06 18:32:48 +0900

    post 속성을 자동으로 반응형 상태로 설정 및 좋아요 버튼에 로딩 애니메이션 추가

--- app/Livewire/LikeButton.php
+++ app/Livewire/LikeButton.php
@@ -4,9 +4,11 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\Attributes\Reactive;
 
 class LikeButton extends Component
 {
+    #[Reactive]
     public Post $post;
 
     public function toggleLike()
