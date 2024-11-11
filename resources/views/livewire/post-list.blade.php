3c29f018089ef3c296de82db5461b27fc284121f
4th-avenue <quebec706@gmail.com>
2024-11-11 19:34:17 +0900

    Post 목록 인기순 정렬 기능 구현

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -17,13 +17,15 @@
             @endif
         </div>
         <div class="flex items-center space-x-4 font-light ">
+            <x-checkbox wire:model.live="popular" />
+            <x-label>Popular</x-label>
             <button class="{{ $sort === 'desc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }} py-4" wire:click="setSort('desc')">Latest</button>
             <button class="{{ $sort === 'asc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500' }} py-4" wire:click="setSort('asc')">Oldest</button>
         </div>
     </div>
     <div class="py-4">
         @foreach ($this->posts as $post)
-            <x-posts.post-item :post="$post" />
+            <x-posts.post-item :key="$post->id" :post="$post" />
         @endforeach
     </div>
 
