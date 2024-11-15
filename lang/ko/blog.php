a8d38404f09c4fa06bf223e4e9ab58dbca7ec37c
4th-avenue <quebec706@gmail.com>
2024-11-15 22:23:50 +0900

    한국어 일부 추가

---
+++ lang/ko/blog.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'containing' => '포함',
+    'popular' => '인기',
+    'latest' => '최신',
+    'oldest' => '가장 오래된',
+    'search' => '검색',
+    'search_placeholder' => '검색 ...',
+    'recommended_topics' => '추천 주제',
+    'min_read' => '읽는데 걸리는 시간'
+
+];
---
+++ lang/ko/home.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'featured_posts' => '추천 포스트',
+    'latest_posts' => '최신 포스트',
+    'more_posts' => '더 많은 포스트',
+
+    'hero' => [
+        'title' => '웰컴 투',
+        'desc' => '우주 최고의 블로그',
+        'cta' => '읽기 시작'
+    ]
+
+];
