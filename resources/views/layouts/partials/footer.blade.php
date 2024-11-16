6571e110da09cce2b6ba7838941dc938b7eceab2
4th-avenue <quebec706@gmail.com>
2024-11-16 13:27:15 +0900

    outhebox/blade-flags 패키지 설치

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,11 @@
-<footer class="text-sm space-x-4 flex items-center border-t border-gray-100 flex-wrap justify-center py-4 ">
-    <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.login') }} </a>
-    <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.profile') }} </a>
-    <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.blog') }} </a>
+<footer class="text-sm flex items-center border-t border-gray-100 flex-wrap justify-between px-4 py-4">
+    <div class="flex space-x-4">
+        <x-flag-country-us class="w-6 h-6" />
+        <x-flag-country-kr class="w-6 h-6" />
+    </div>
+    <div class="flex space-x-4">
+        <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.login') }} </a>
+        <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.profile') }} </a>
+        <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.blog') }} </a>
+    </div>
 </footer>
