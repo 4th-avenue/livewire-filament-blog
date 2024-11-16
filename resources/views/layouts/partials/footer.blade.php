a982de453cb7d7cf55b6a6a129074d86204aeaae
4th-avenue <quebec706@gmail.com>
2024-11-16 14:34:44 +0900

    언어 전환 기능 구현

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="text-sm flex items-center border-t border-gray-100 flex-wrap justify-between px-4 py-4">
     <div class="flex space-x-4">
-        <x-flag-country-us class="w-6 h-6" />
-        <x-flag-country-kr class="w-6 h-6" />
+        <a href="{{ route('locale', 'en') }}"><x-flag-country-us class="w-6 h-6" /></a>
+        <a href="{{ route('locale', 'ko') }}"><x-flag-country-kr class="w-6 h-6" /></a>
     </div>
     <div class="flex space-x-4">
         <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.login') }} </a>
